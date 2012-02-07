2705f669eb42eab743df8a3dd5713fd270c68f04
Matt <Matt@Matt-PC>
2012-02-06 20:09:30 -0500

    Deleted last year's targetting files.

--- chopshop12/TargetingInfo.h
+++ chopshop12/TargetingInfo.h
@@ -15,7 +15,7 @@
 
 #define DISTANCECALIBRATION 4 //THIS IS TEMPORARY! NOT THE ACTUAL VALUE!distance the robot is from the hoop when the top vision target is at the very top of the image
 #define GRAVITY 32.17 //ft/s^2
-#define PI 3.14159265
+#define PI 3.14159265358979
 #define TOPDELTAY 5 //THIS IS TEMPORARY! NOT THE ACTUAL VALUE!vertical distance from launcher to top hoop
 #define LANGLE 0.872664626 //launch angle = 50 degrees
 
