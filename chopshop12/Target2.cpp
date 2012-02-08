e64ea51e93761762edf27d5ff583520cde6d2155
M <devnull@localhost>
2012-02-07 20:35:09 -0500

    Image Processing: Adjusted image processing for bad camera.

--- chopshop12/Target2.cpp
+++ chopshop12/Target2.cpp
@@ -14,6 +14,8 @@
 static bool FailCheck(int Returned, char* Description);		//Print out errors
 static int Countup(Image* ImageToCount);					//Print out number of particles
 
+#define BAD_CAMERA (true)
+
 static int ReturnReport(
 		Image* ProcessedImage, 
 		int numParticles, 
@@ -33,6 +35,7 @@ int ProcessMyImage(Image* CameraInput, ParticleAnalysisReport* ParticleRep, int 
 	Image* ProcessedImage = frcCreateImage(IMAQ_IMAGE_U8);
 	DPRINTF(LOG_INFO, "\nLOOK HERE\n");
 	
+#if (!BAD_CAMERA)
 	/*Step 1: Color Threshold 
 	(Red: 0-255, Green: 174-255,  Blue: 210-255)
 	int imaqColorThreshold(Image* dest, const Image* source, int replaceValue, ColorMode mode, const Range* plane1Range, const Range* plane2Range, const Range* plane3Range); */
@@ -115,14 +118,65 @@ int ProcessMyImage(Image* CameraInput, ParticleAnalysisReport* ParticleRep, int 
 	
 	
 	/* Step 7.0: Check particles */
-	int numP = Countup(ProcessedImage);
+	/*This will be commented out until I remember what it's supposed to do...
+	 * int numP = Countup(ProcessedImage);
 	ParticleAnalysisReport PARTest[numP];
 	for (int h=0; h<numP; h++)
 	{
 		frcParticleAnalysis(ProcessedImage, h, &PARTest[h]);
 		TPRINTF(LOG_INFO, "Particle: %i   \t%i", PARTest[h].center_mass_x, PARTest[h].center_mass_y);
 	}
+	*/
+#endif
 	
+#if BAD_CAMERA
+	/*Step 1: Color Threshold */
+		/* Setup Threshold Values */
+		const Range RR = {0  ,255};
+		const Range GR = {0  ,255};
+		const Range BR = {150,255};
+		if(FailCheck(imaqColorThreshold(ProcessedImage, CameraInput, 255, IMAQ_HSL, &RR, &GR, &BR), "Color Threshold Failed %i")) {return 0; } else {DPRINTF(LOG_INFO, "Thresholded");}
+		if (!Countup(ProcessedImage)) return 0;
+		
+		/*Step 2: Basic Morphology Dilation
+		int imaqMorphology(Image* dest, Image* source, MorphologyMethod method, const StructuringElement* structuringElement); */
+		/* Setup for Morphology*/
+			static int kernelvalues[9] = {1,1,1,1,1,1,1,1,1};
+			const StructuringElement StructEle = {3, 3, FALSE, &kernelvalues[0]};
+			imaqMorphology(ProcessedImage, ProcessedImage, IMAQ_DILATE, &StructEle);
+		if (FailCheck(imaqMorphology(ProcessedImage, ProcessedImage, IMAQ_DILATE, &StructEle), "Dilation 1 %f")) {return 0; } else {DPRINTF(LOG_INFO, "Dilated");}
+		free(StructEle.kernel);
+		Countup(ProcessedImage);
+		
+		/* Step 3: Basic Morphology Erosion (to clean up) */
+		imaqMorphology(ProcessedImage, ProcessedImage, IMAQ_ERODE, &StructEle);
+		imaqMorphology(ProcessedImage, ProcessedImage, IMAQ_ERODE, &StructEle);
+		
+		
+		/*Step 4: Particle Filter (Area: min.5%, max100% of image)
+		int imaqParticleFilter4(Image* dest, Image* source, const ParticleFilterCriteria2* criteria, int criteriaCount, const ParticleFilterOptions2* options, const ROI* roi, int* numParticles); */
+		/* Setup for Particle Filter */
+		int IMAQheight;
+		int IMAQwidth;
+		imaqGetImageSize(ProcessedImage, &IMAQwidth, &IMAQheight);
+		ParticleFilterCriteria2 CRIT[3] = {{IMAQ_MT_AREA,             100  , IMAQwidth*IMAQheight, FALSE, FALSE}, 
+				                           {IMAQ_MT_COMPACTNESS_FACTOR, 0.0, 0.7, FALSE, FALSE},
+	    								   {IMAQ_MT_COMPACTNESS_FACTOR, 0.7, 1.0, FALSE, FALSE}};
+		const ParticleFilterOptions2 OPTS = {FALSE, FALSE, FALSE, TRUE};
+		int NP;
+		if(FailCheck(imaqParticleFilter4(ProcessedImage, ProcessedImage, &CRIT[0], 2, &OPTS, NULL, &NP), "Filter Particles 1 %i")) {return 0; } else {DPRINTF(LOG_INFO, "Filtered");}
+		Countup(ProcessedImage);
+		
+		/* Step 5: Convex hull it */
+		if(FailCheck(imaqConvexHull(ProcessedImage, ProcessedImage, TRUE), "Convex Hull")) 
+		{return 0; } 
+		else 
+		{DPRINTF(LOG_INFO, "Convexed");}
+		
+		/* Step 6: Take out the particles that aren't filled in enough */
+		if(FailCheck(imaqParticleFilter4(ProcessedImage, ProcessedImage, &CRIT[2], 1, &OPTS, NULL, &NP), "Filter Particles 2 %i")) {return 0; } else {DPRINTF(LOG_INFO, "Filtered");}
+		Countup(ProcessedImage);
+#endif
 	/* Step 7: Apply logic */
 	int numParticles=Countup(ProcessedImage);
 	TPRINTF(LOG_INFO, "Number particles found: %i", numParticles);
@@ -146,7 +200,36 @@ int ProcessMyImage(Image* CameraInput, ParticleAnalysisReport* ParticleRep, int 
 	}
 	else if(numParticles>4)
 	{
+		int BestDVIndex;
+		double BestDV=0;
+		TPRINTF(LOG_INFO, "More than 4 found!");
+		Countup(ProcessedImage);
+		for (int i = 0; i < numParticles; i++) 
+		{
+			double testmeasure;
+			imaqMeasureParticle(ProcessedImage, i,  0, IMAQ_MT_AREA, &testmeasure);
+			if (testmeasure > BestDV)
+			{ BestDV=testmeasure; BestDVIndex = i; }
+		}
+		ParticleFilterCriteria2 CRIT2 = {IMAQ_MT_AREA, (.9)*BestDV, IMAQwidth*IMAQheight, FALSE, FALSE};
+		if(FailCheck(imaqParticleFilter4(ProcessedImage, ProcessedImage, &CRIT2, 1, &OPTS, NULL, &NP), "Filter Particles 1 %i")) {return 0; } else {DPRINTF(LOG_INFO, "Filtered");}
+		numParticles= Countup(ProcessedImage);
 		
+		ReturnReport(ProcessedImage, numParticles, TOP_MOST,    &ParticleRep[TOP_MOST]   );
+		ReturnReport(ProcessedImage, numParticles, LEFT_MOST,   &ParticleRep[LEFT_MOST]  );
+		ReturnReport(ProcessedImage, numParticles, RIGHT_MOST,  &ParticleRep[RIGHT_MOST] );
+		ReturnReport(ProcessedImage, numParticles, BOTTOM_MOST, &ParticleRep[BOTTOM_MOST]);
+		
+		TPRINTF(LOG_INFO, "TOP:    %f   \t%f", ParticleRep[TOP_MOST].center_mass_x_normalized,    ParticleRep[TOP_MOST].center_mass_y_normalized);
+		TPRINTF(LOG_INFO, "LEFT:   %f   \t%f", ParticleRep[LEFT_MOST].center_mass_x_normalized,   ParticleRep[LEFT_MOST].center_mass_y_normalized);
+		TPRINTF(LOG_INFO, "RIGHT:  %f   \t%f", ParticleRep[RIGHT_MOST].center_mass_x_normalized,  ParticleRep[RIGHT_MOST].center_mass_y_normalized);
+		TPRINTF(LOG_INFO, "BOTTOM: %f   \t%f", ParticleRep[BOTTOM_MOST].center_mass_x_normalized, ParticleRep[BOTTOM_MOST].center_mass_y_normalized);
+	
+		TPRINTF(LOG_INFO, "TOP:    %i   \t%i", ParticleRep[TOP_MOST].center_mass_x,    ParticleRep[TOP_MOST].center_mass_y);
+		TPRINTF(LOG_INFO, "LEFT:   %i   \t%i", ParticleRep[LEFT_MOST].center_mass_x,   ParticleRep[LEFT_MOST].center_mass_y);
+		TPRINTF(LOG_INFO, "RIGHT:  %i   \t%i", ParticleRep[RIGHT_MOST].center_mass_x,  ParticleRep[RIGHT_MOST].center_mass_y);
+		TPRINTF(LOG_INFO, "BOTTOM: %i   \t%i", ParticleRep[BOTTOM_MOST].center_mass_x, ParticleRep[BOTTOM_MOST].center_mass_y);
+	
 	}
 	
 	return numParticles;
