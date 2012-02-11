2f7127c02c7e3b4d98edde49d58eeb8c4690821f
Matt <Matt@Matt-PC>
2012-02-11 14:04:22 -0500

    more realistic estimates of claibration values

--- chopshop12/Tasks.h
+++ chopshop12/Tasks.h
@@ -6,9 +6,9 @@
 // No need for pointers, or anything else, if it's derived from Team166Task
 
 //ballCollector166 ballCollector;
-bridgeManipulator166 bridgeManipulator;	
+//bridgeManipulator166 bridgeManipulator;	
 //Turret166 TurretObject;
 //Shooter ShooterObject;
-//CameraTask CameraObject;
+CameraTask CameraObject;
 //BallCameraTask BallCameraObject;
 //Drive166 DriveObject;
